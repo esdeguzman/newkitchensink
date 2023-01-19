9878a59583a05409812574f30261783b736b2771
Esmeraldo De Guzman Jr <deguzman.esmeraldo@gmail.com>
2023-01-19 15:56:55 +0800

    Converted config file to ES module

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,7 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  projectId: "wore59",
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: "https://example.cypress.io",
+export const projectId = "wore59";
+export const e2e = {
+  setupNodeEvents(on, config) {
+    // implement node event listeners here
   },
-});
+  baseUrl: "https://example.cypress.io",
+};
